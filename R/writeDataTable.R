8ff8d39e61e9f9e7c75592235fe24fb9338e2598
awalker89 <alexander.walker1989@gmail.com>
2014-10-07 20:09:11 +1100

    change withFilter to filter (writeData consistent)

--- R/writeDataTable.R
+++ R/writeDataTable.R
@@ -13,7 +13,7 @@
 #' @param tableStyle Any excel table style name or "none".
 #' @param tableName name of table in workbook. The table name must be unique.
 #' @param headerStyle Custom style to apply to column names.
-#' @param withFilter If TRUE, columns with have filters in the first row.
+#' @param filter If TRUE, columns with have filters in the first row.
 #' @details columns of x with class Date/POSIXt, currency, accounting, 
 #' hyperlink, percentage are automatically styled as dates, currency, accounting,
 #' hyperlinks, percentages respectively.
@@ -49,7 +49,7 @@
 #' 
 #' ## Additional headerStyling and remove filters
 #' writeDataTable(wb, sheet = 1, x = iris, startCol = 7, headerStyle = createStyle(textRotation = 45),
-#' withFilter = FALSE)
+#' filter = FALSE)
 #' 
 #' saveWorkbook(wb, "writeDataTableExample.xlsx", overwrite = TRUE)
 writeDataTable <- function(wb, sheet, x,
@@ -61,7 +61,7 @@ writeDataTable <- function(wb, sheet, x,
                            tableStyle = "TableStyleLight9",
                            tableName = NULL,
                            headerStyle= NULL,
-                           withFilter = TRUE){
+                           filter = TRUE){
   
     
   if(!is.null(xy)){
@@ -77,7 +77,7 @@ writeDataTable <- function(wb, sheet, x,
   if(!is.logical(colNames)) stop("colNames must be a logical.")
   if(!is.logical(rowNames)) stop("rowNames must be a logical.")
   if(!is.null(headerStyle) & !"Style" %in% class(headerStyle)) stop("headerStyle must be a style object or NULL.")
-  if(!is.logical(withFilter)) stop("withFilter must be a logical.")
+  if(!is.logical(filter)) stop("filter must be a logical.")
   
   if(is.null(tableName)){
     tableName <- paste0("Table", as.character(length(wb$tables) + 3L))
@@ -195,6 +195,6 @@ writeDataTable <- function(wb, sheet, x,
   colNames <- replaceIllegalCharacters(colNames)
   
   ## create table.xml and assign an id to worksheet tables
-  wb$buildTable(sheet, colNames, ref, showColNames, tableStyle, tableName, withFilter[1])
+  wb$buildTable(sheet, colNames, ref, showColNames, tableStyle, tableName, filter[1])
   
 }
