5ca858006503d37f2cc5ca0b5cee64fe9cd6f6c2
lbraglia <lbraglia@gmail.com>
2014-10-26 14:14:02 +0100

    an alternative to View

---
+++ R/view.R
@@ -0,0 +1,33 @@
+#'
+#' Alternative to View that uses openXL
+#'
+#' Alternative to \code{\link{View}} that invokes a spreasheet viewer on
+#' openxlsx's \code{\link{writeData}} handled objects. 
+#'
+#' @param ... writeData handled objects
+#' @examples \dontrun{
+#' view(Indometh, iris)
+#' }
+#' @export
+view <- function(...)  {
+  wb <- openxlsx::createWorkbook()
+  objList <- list(...)
+  objNames <- lapply(as.list(match.call(expand.dots = TRUE))[-1],
+                     as.character)
+  mapply(addWorksheet,
+         sheetName = objNames,
+         MoreArgs = list(wb = wb))
+  mapply(writeData,
+         sheet = objNames,
+         x = objList,
+         MoreArgs = list(wb = wb)) 
+  openxlsx::openXL(wb)
+}
+
+
+
+
+
+
+
+  
