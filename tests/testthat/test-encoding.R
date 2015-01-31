1136d189d3ff22c463fe1190b8fafb05c1b0f892
Alexander Walker <Alexander.Walker1989@gmail.com>
2015-02-01 10:04:12 +1100

    Encoding unit tests

---
+++ tests/testthat/test-encoding.R
@@ -0,0 +1,36 @@
+
+
+
+context("Encoding Tests")
+
+
+
+test_that("Write read encoding equality", {
+  
+  tempFile <- file.path(tempdir(), "temp.xlsx")
+  
+  wb <- createWorkbook()
+  for(i in 1:4)
+    addWorksheet(wb, sprintf('Sheet %s', i))
+  
+  df <- data.frame("X" = c("测试", "一下"), stringsAsFactors = FALSE)
+  writeData(wb, sheet = 1, x = df)
+  
+  saveWorkbook(wb, tempFile, overwrite = TRUE)
+  
+  x <- read.xlsx(tempFile)
+  expect_equal(x, df)
+
+  
+  x <- read.xlsx(wb)
+  expect_equal(x, df)
+  
+  
+  unlink(tempFile, recursive = TRUE, force = TRUE)
+  rm(wb)
+  
+})
+
+
+
+
