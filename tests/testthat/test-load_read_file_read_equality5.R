9861d99edad06ff39c134e518bdfc9cedfe02976
Alexander Walker <Alexander.Walker1989@gmail.com>
2015-03-24 22:28:19 +1100

    Split out test

---
+++ tests/testthat/test-load_read_file_read_equality5.R
@@ -0,0 +1,133 @@
+
+
+
+context("Reading from workbook is identical to reading from file 5")
+
+
+
+test_that("Reading example workbook", {
+  
+  xlsxFile <- system.file("readTest.xlsx", package = "openxlsx")
+  wb <- loadWorkbook(xlsxFile)
+  
+  ## sheet 1
+  sheet <- 1
+  x <- read.xlsx(xlsxFile, sheet)
+  y <- read.xlsx(wb, sheet)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, startRow = 3, colNames = FALSE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, startRow = 3, colNames = FALSE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = TRUE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = TRUE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = FALSE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = FALSE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = FALSE, detectDates = FALSE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = FALSE, detectDates = FALSE)
+  expect_equal(x, y)
+  
+  
+  x <- read.xlsx(xlsxFile, sheet, colNames = FALSE, detectDates = FALSE, cols = 2:4)
+  y <- read.xlsx(wb, sheet, colNames = FALSE, detectDates = FALSE, cols = 2:4)
+  expect_equal(x, y)
+  
+  
+  
+  
+  ## sheet 2
+  sheet <- 2
+  x <- read.xlsx(xlsxFile, sheet)
+  y <- read.xlsx(wb, sheet)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, startRow = 3, colNames = FALSE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, startRow = 3, colNames = FALSE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = TRUE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = TRUE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = FALSE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = FALSE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = FALSE, detectDates = FALSE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = FALSE, detectDates = FALSE)
+  expect_equal(x, y)
+  
+  
+  x <- read.xlsx(xlsxFile, sheet, colNames = FALSE, detectDates = FALSE, cols = 2:4)
+  y <- read.xlsx(wb, sheet, colNames = FALSE, detectDates = FALSE, cols = 2:4)
+  expect_equal(x, y)
+  
+  
+  
+  ## sheet 3
+  sheet <- 3
+  x <- read.xlsx(xlsxFile, sheet)
+  y <- read.xlsx(wb, sheet)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, startRow = 3, colNames = FALSE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, startRow = 3, colNames = FALSE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = TRUE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = TRUE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = FALSE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = FALSE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = FALSE, detectDates = FALSE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = FALSE, detectDates = FALSE)
+  expect_equal(x, y)
+  
+  
+  x <- read.xlsx(xlsxFile, sheet, colNames = FALSE, detectDates = FALSE, cols = 2:4)
+  y <- read.xlsx(wb, sheet, colNames = FALSE, detectDates = FALSE, cols = 2:4)
+  expect_equal(x, y)
+  
+  
+  
+  ## sheet 4
+  sheet <- 4
+  x <- read.xlsx(xlsxFile, sheet)
+  y <- read.xlsx(wb, sheet)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, startRow = 3, colNames = FALSE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, startRow = 3, colNames = FALSE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = TRUE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = TRUE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = FALSE, detectDates = TRUE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = FALSE, detectDates = TRUE)
+  expect_equal(x, y)
+  
+  x <- read.xlsx(xlsxFile, sheet, rows = 2:4, colNames = FALSE, detectDates = FALSE)
+  y <- read.xlsx(wb, sheet, rows = 2:4, colNames = FALSE, detectDates = FALSE)
+  expect_equal(x, y)
+  
+  
+  x <- read.xlsx(xlsxFile, sheet, colNames = FALSE, detectDates = FALSE, cols = 2:4)
+  y <- read.xlsx(wb, sheet, colNames = FALSE, detectDates = FALSE, cols = 2:4)
+  expect_equal(x, y)
+  
+  
+})
