cbf4f6bad01cf254fd26e79d04c7d3fce4e9d14b
Alex Walker <Alexander.Walker1989@gmail.com>
2016-12-10 18:23:04 +1100

    add more tests

--- tests/testthat/test-read_from_created_wb.R
+++ tests/testthat/test-read_from_created_wb.R
@@ -2,7 +2,7 @@
 
 
 
-context("Reading from workbook is identical to reading from file")
+context("Reading from written workbook is identical to reading from file 1")
 
 
 
@@ -143,10 +143,10 @@ test_that("Empty workbook", {
 
 
 
-test_that("NAs and NaN values", {
+test_that("Reading NAs and NaN values", {
   
   fileName <- file.path(tempdir(), "NaN.xlsx")
-
+  
   ## data
   a <- data.frame("X" = c(-pi/0, NA, NaN),
                   "Y" = letters[1:3], 
@@ -168,12 +168,38 @@ test_that("NAs and NaN values", {
   
   saveWorkbook(wb, file = fileName, overwrite = TRUE)
   
+  ## from file
+  expected_df <- structure(list(X = c(NA_real_, NA_real_, NA_real_)
+                                , Y = c("a", "b", "c")
+                                , Z = c(NA, 99, NA)
+                                , Z2 = c(1, NA, NA))
+                           , .Names = c("X", "Y", "Z", "Z2")
+                           , row.names = c(NA, 3L), class = "data.frame")
+  
+  expect_equal(read.xlsx(fileName), expected_df)
+  
+  ## from workbook
+  expected_df <- structure(list(X = c(NA_real_, NA_real_, NA_real_)
+                                , Y = c("a", "b", "c")
+                                , Z = c(NA, 99, NA)
+                                , Z2 = c(1, NA, NA))
+                           , .Names = c("X", "Y", "Z", "Z2")
+                           , row.names = c(NA, 3L), class = "data.frame")
+  
+  expect_equal(read.xlsx(wb), expected_df)
+  
+  
+   
   ## keepNA = FALSE
+  expect_equal(read.xlsx(wb), read.xlsx(fileName))  
   expect_equal(b, read.xlsx(wb))  
   expect_equal(b, read.xlsx(fileName)) 
   
-
   ## keepNA = TRUE
+  
+  expect_equal(read.xlsx(wb), expected_df)
+  expect_equal(read.xlsx(fileName), expected_df)
+  
   expect_equal(b, read.xlsx(wb, sheet = 2))  
   expect_equal(b, read.xlsx(fileName, sheet = 2)) 
   
@@ -185,7 +211,134 @@ test_that("NAs and NaN values", {
 
 
 
+test_that("Reading from new workbook 2 ", {
+  
+  ## data
+  genDf <- function(){
+    data.frame("Date" = Sys.Date()-0:4,
+               "Logical" = c(TRUE, FALSE, TRUE, TRUE, FALSE),
+               "Currency" = -2:2,
+               "Accounting" = -2:2,
+               "hLink" = "http://cran.r-project.org/", 
+               "Percentage" = seq(-1, 1, length.out=5),
+               "TinyNumber" = runif(5) / 1E9, stringsAsFactors = FALSE)
+  }
+  
+  df <- genDf()
+  
+  class(df$Currency) <- "currency"
+  class(df$Accounting) <- "accounting"
+  class(df$hLink) <- "hyperlink"
+  class(df$Percentage) <- "percentage"
+  class(df$TinyNumber) <- "scientific"
+  
+  options("openxlsx.dateFormat" = NULL)
+  
+  fileName <- file.path(tempdir(), "allClasses.xlsx")
+  wb <- write.xlsx(df, file = fileName, overwrite = TRUE)
+  
+  x <- read.xlsx(wb, sheet = 1, detectDates = FALSE)
+  x[[1]] <- convertToDate(x[[1]])
+  expect_equal(object = x, expected = genDf(), check.attributes = FALSE)
+  
+  
+  x <- read.xlsx(wb, sheet = 1, detectDates = TRUE)
+  expect_equal(object = x, expected = genDf(), check.attributes = FALSE)
+  
+  unlink(fileName, recursive = TRUE, force = TRUE)
+  
+})
 
+
+
+
+
+test_that("Reading from new workbook cols/rows", {
+  
+  wb <- createWorkbook()
+  for(i in 1:4)
+    addWorksheet(wb, sprintf('Sheet %s', i))
+  
+  tempFile <- file.path(tempdir(), "temp.xlsx")
+  
+  ## 1
+  writeData(wb, sheet = 1, x = mtcars, colNames = TRUE, rowNames = FALSE)
+  saveWorkbook(wb, tempFile, overwrite = TRUE)
+  
+  cols <- 1:3
+  rows <- 1:10
+  x <- read.xlsx(wb, 1, colNames = TRUE, rowNames = FALSE, rows = rows, cols = cols)
+  y <- read.xlsx(tempFile, 1, colNames = TRUE, rowNames = FALSE, rows = rows, cols = cols)
+  
+  df <- mtcars[sort((rows-1)[(rows-1) <= nrow(mtcars)]), sort(cols[cols <= ncol(mtcars)])]
+  rownames(df) <- 1:nrow(df)
+  
+  expect_equal(object = x, expected = y)
+  expect_equal(object = x, expected = df)
+  
+  
+  
+  ## 2
+  writeData(wb, sheet = 2, x = mtcars, colNames = TRUE, rowNames = FALSE, startRow = 10, startCol = 5)
+  saveWorkbook(wb, tempFile, overwrite = TRUE)
+  
+  cols <- 1:300
+  rows <- 1:1000
+  x <- read.xlsx(wb, sheet = 2, colNames = TRUE, rowNames = FALSE, rows = rows, cols = cols)
+  y <- read.xlsx(tempFile, sheet = 2, colNames = TRUE, rowNames = FALSE, rows = rows, cols = cols)
+  
+  # 
+  expect_equal(object = mtcars, expected = x, check.attributes = FALSE)
+  expect_equal(object = x, expected = y, check.attributes = TRUE)
+  expect_equal(object = colnames(mtcars), expected = colnames(x), check.attributes = FALSE)
+  
+  
+  cols <- 1:3
+  rows <- 12:13
+  x <- read.xlsx(wb, sheet = 2, colNames = TRUE, rowNames = FALSE, rows = rows, cols = cols)
+  y <- read.xlsx(tempFile, sheet = 2, colNames = TRUE, rowNames = FALSE, rows = rows, cols = cols)
+  
+  
+  expect_equal(object = NULL, expected = x, check.attributes = FALSE)
+  expect_equal(object = NULL, expected = y, check.attributes = TRUE)
+  
+  
+  
+  ## 3
+  writeData(wb, sheet = 3, x = mtcars, colNames = TRUE, rowNames = FALSE)
+  saveWorkbook(wb, tempFile, overwrite = TRUE)
+  
+  cols <- c(2, 4, 6)
+  rows <- seq(1, 31, by = 2)
+  
+  x <- read.xlsx(wb, sheet = 3, colNames = TRUE, rowNames = FALSE, rows = rows, cols = cols)
+  y <- read.xlsx(tempFile, sheet = 3, colNames = TRUE, rowNames = FALSE, rows = rows, cols = cols)
+  
+  df <- mtcars[sort((rows-1)[(rows-1) <= nrow(mtcars)]), sort(cols[cols <= ncol(mtcars)])]
+  rownames(df) <- 1:nrow(df)
+  
+  expect_equal(object = x, expected = y, check.attributes = FALSE)
+  expect_equal(object = df, expected = x, check.attributes = FALSE)
+  
+  
+  
+  ## 4
+  writeData(wb, sheet = 4, x = mtcars, colNames = TRUE, rowNames = TRUE)
+  saveWorkbook(wb, tempFile, overwrite = TRUE)
+  
+  cols <- c(1, 6, 12)
+  rows <- seq(1, 31, by = 2)
+  x <- read.xlsx(wb, sheet = 4, colNames = TRUE, rowNames = TRUE, rows = rows, cols = cols)  
+  y <- read.xlsx(tempFile, sheet = 4, colNames = TRUE, rowNames = TRUE, rows = rows, cols = cols)  
+  
+  df <- mtcars[sort((rows-1)[(rows-1) <= nrow(mtcars)]),cols[-1]-1]
+  expect_equal(object = x, expected = y, check.attributes = FALSE)
+  expect_equal(object = df, expected = x, check.attributes = FALSE)
+  
+  rm(wb)
+  unlink(tempFile, recursive = TRUE, force = TRUE)
+  
+})
 
 
 
