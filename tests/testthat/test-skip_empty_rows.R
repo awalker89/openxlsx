efb0c385df2dfdf7d7c5b2e621a4e46b0a44a507
Alex Walker <Alexander.Walker1989@gmail.com>
2017-02-28 18:40:25 +1100

    add skip row/cols tests for Workbook object

--- tests/testthat/test-skip_empty_rows.R
+++ tests/testthat/test-skip_empty_rows.R
@@ -128,7 +128,7 @@ test_that("skip empty cols", {
 
 
 
-test_that("Version 4 fixes", {
+test_that("Version 4 fixes from File", {
 
   fl <- system.file("readTest.xlsx", package = "openxlsx")
   
@@ -237,5 +237,121 @@ test_that("Version 4 fixes", {
   
 
 })
+
+
+
+
+
+
+test_that("Version 4 fixes from Workbook Objects", {
+  
+  fl <- loadWorkbook(system.file("readTest.xlsx", package = "openxlsx"))
+  
+  
+  x <- read.xlsx(xlsxFile = fl, sheet = 4, skipEmptyCols = TRUE, skipEmptyRows = TRUE, colNames = FALSE)
+  expect_equal(nrow(x), 5L)
+  expect_equal(ncol(x), 4L)
+  
+  x <- read.xlsx(xlsxFile = fl, sheet = 4, skipEmptyCols = TRUE, skipEmptyRows = TRUE, colNames = TRUE)
+  expect_equal(nrow(x), 5L - 1L)
+  expect_equal(ncol(x), 4L)
+  
+  
+  ##############################################################
+  ## FALSE FALSE FALSE
+  
+  x <- read.xlsx(xlsxFile = fl, sheet = 4, skipEmptyCols = FALSE, skipEmptyRows = FALSE, colNames = FALSE)
+  expect_equal(nrow(x), 6L)
+  expect_equal(ncol(x), 8L)
+  
+  ## NA columns
+  expect_true(all(is.na(x$X1)))
+  expect_true(all(is.na(x$X2)))
+  expect_true(all(is.na(x$X3)))
+  expect_true(all(is.na(x$X7)))
+  
+  ## NA rows
+  expect_true(all(is.na(x[3,])))
+  
+  
+  
+  ##############################################################
+  ## FALSE FALSE TRUE
+  
+  x <- read.xlsx(xlsxFile = fl, sheet = 4, skipEmptyCols = FALSE, skipEmptyRows = FALSE, colNames = TRUE)
+  expect_equal(nrow(x), 6L - 1L)
+  expect_equal(ncol(x), 8L)
+  
+  
+  ## NA columns
+  expect_true(all(is.na(x$X1)))
+  expect_true(all(is.na(x$X2)))
+  expect_true(all(is.na(x$X3)))
+  expect_true(all(is.na(x$X7)))
+  
+  ## NA rows
+  expect_true(all(is.na(x[2,])))
+  
+  
+  
+  ##############################################################
+  ## FALSE TRUE FALSE
+  
+  x <- read.xlsx(xlsxFile = fl, sheet = 4, skipEmptyCols = FALSE, skipEmptyRows = TRUE, colNames = FALSE)
+  expect_equal(nrow(x), 5L)
+  expect_equal(ncol(x), 8L)
+  
+  ## NA columns
+  expect_true(all(is.na(x$X1)))
+  expect_true(all(is.na(x$X2)))
+  expect_true(all(is.na(x$X3)))
+  expect_true(all(is.na(x$X7)))
+  
+  
+  
+  
+  ##############################################################
+  ## FALSE TRUE TRUE
+  
+  x <- read.xlsx(xlsxFile = fl, sheet = 4, skipEmptyCols = FALSE, skipEmptyRows = TRUE, colNames = TRUE)
+  expect_equal(nrow(x), 5L - 1L)
+  expect_equal(ncol(x), 8L)
+  
+  
+  ## NA columns
+  expect_true(all(is.na(x$X1)))
+  expect_true(all(is.na(x$X2)))
+  expect_true(all(is.na(x$X3)))
+  expect_true(all(is.na(x$X7)))
+  
+  
+  
+  ##############################################################
+  ## TRUE FALSE FALSE
+  
+  x <- read.xlsx(xlsxFile = fl, sheet = 4, skipEmptyCols = TRUE, skipEmptyRows = FALSE, colNames = FALSE)
+  expect_equal(nrow(x), 6L)
+  expect_equal(ncol(x), 4L)
+  
+  ## NA rows
+  expect_true(all(is.na(x[3,])))
+  
+  
+  
+  ##############################################################
+  ## TRUE FALSE TRUE
+  
+  x <- read.xlsx(xlsxFile = fl, sheet = 4, skipEmptyCols = TRUE, skipEmptyRows = FALSE, colNames = TRUE)
+  expect_equal(nrow(x), 6L - 1L)
+  expect_equal(ncol(x), 4L)
+  
+  
+  ## NA rows
+  expect_true(all(is.na(x[2,])))
+  
+  
+  
+})
+
 
 
