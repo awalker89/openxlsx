61901d01705fb6b934048be5ace41de90ef59dde
awalker89 <Alexander.Walker1989@gmail.com>
2018-05-26 22:02:33 +1000

    fix test

--- tests/testthat/test-writing_posixct.R
+++ tests/testthat/test-writing_posixct.R
@@ -24,18 +24,17 @@ test_that("Writing Posixct wikth writeData & writeDataTable", {
   writeData(wb, "writeData", df, startCol = 2, startRow = 3, rowNames = FALSE)
   writeDataTable(wb, "writeDataTable", df, startCol = 2, startRow = 3)
   
-  wd <- wb$worksheets[[1]]$sheet_data$v
-  wdt <- wb$worksheets[[2]]$sheet_data$v
+  wd <- as.numeric(wb$worksheets[[1]]$sheet_data$v)
+  wdt <- as.numeric(wb$worksheets[[2]]$sheet_data$v)
   
   
-  expected <- c("0", "1", "42885.354166666664", "2", "42885.357638888891", 
-                "3", "42885.361111111117", "4", "42885.364583333336", "5", "42885.375000000007", 
-                "6", "42885.395833333336", "7", "42885.437500000007", "8", "42885.479166666664", 
-                "9", "42885.520833333336", "10", "42885.687500000007", "11", 
-                "42885.854166666664", "12", "42886.354166666664", "13")
+  expected <- c(0, 1, 42885.3541666667, 2, 42885.3576388889, 3, 42885.3611111111, 
+                4, 42885.3645833333, 5, 42885.375, 6, 42885.3958333333, 7, 42885.4375, 
+                8, 42885.4791666667, 9, 42885.5208333333, 10, 42885.6875, 11, 
+                42885.8541666667, 12, 42886.3541666667, 13)
   
-  expect_equal(object = wd, expected = expected)
-  expect_equal(object = wdt, expected = expected)
+  expect_equal(object = round(wd, 12), expected = expected)
+  expect_equal(object = round(wdt, 12), expected = expected)
   expect_equal(object = wd, expected = wdt)
   
   options("openxlsx.datetimeFormat" = "yyyy-mm-dd hh:mm:ss")
